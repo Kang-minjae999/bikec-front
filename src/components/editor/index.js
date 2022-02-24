1403de82118ac543adff1f8198151ad873cdc106
Kang-minjae999 <96714650+Kang-minjae999@users.noreply.github.com>
2022-02-24 12:05:55 +0900

    asdasdsa
    dasdsadasd

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -89,7 +89,6 @@ export default function Editor({
           });
           console.log(response.data.data);
           seturl(response.data.data);
-          const url2 = response.data.data
 
           // console.log(seturl);
             /* const range = QuillRef.current?.getEditor().getSelection()?.index;
@@ -101,25 +100,24 @@ export default function Editor({
             quill?.clipboard.dangerouslyPasteHTML(range, `<img src=${url2} alt='asas' />`);
           } 
           return { ...response, success: true }; */
+          if(url !== ''){
 
-          const editor = QuillRef.current.getEditor(); // 에디터 객체 가져오기
-          // 1. 에디터 root의 innerHTML을 수정해주기
-          // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
-          // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
-          // editor.root.innerHTML =
-          //   editor.root.innerHTML + `<img src=${IMG_URL} /><br/>`; // 현재 있는 내용들 뒤에 써줘야한다.
+            const editor = QuillRef.current.getEditor(); 
     
-          // 2. 현재 에디터 커서 위치값을 가져온다
-          const range = editor.getSelection();
-          // 가져온 위치에 이미지를 삽입한다
-          editor.insertEmbed(range.index, 'image', url2);
-
-        } catch (error) {
+            // 2. 현재 에디터 커서 위치값을 가져온다
+            const range = editor.getSelection();
+            // 가져온 위치에 이미지를 삽입한다
+            editor.insertEmbed(range.index, 'image', `${url}`);
+          }
+          
+        } 
+        
+        catch (error) {
           const err = error;
           return { ...err.response, success: false };
         }
       }
-      return {};
+      return ''
     };
   };
 
