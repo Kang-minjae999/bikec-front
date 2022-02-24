65dabea0d1a31811c2d66f17f4dfa551fa69af34
Kang-minjae999 <96714650+Kang-minjae999@users.noreply.github.com>
2022-02-24 12:45:09 +0900

    asdasd
    adasdsad

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import ReactQuill from 'react-quill';
-import { useMemo, useRef, useState } from 'react';
+import { useMemo, useRef, useState, useEffect } from 'react';
 // @mui
 import { styled } from '@mui/material/styles';
 import { Box } from '@mui/material';
@@ -56,7 +56,7 @@ export default function Editor({
   ...other
 }) {
   const QuillRef = useRef();
-  const [url, seturl] = useState('');
+  const [url, seturl] = useState();
 
   const imageHandler = () => {
     const accessToken = window.localStorage.getItem('accessToken');
@@ -66,7 +66,6 @@ export default function Editor({
 
     input.setAttribute('type', 'file');
     input.setAttribute('accept', '*/*');
-    input.setAttribute('name', 'imageFile');
     // input.setAttribute('processData', false);
     // input.setAttribute('contentType', false);
     input.click();
@@ -78,7 +77,17 @@ export default function Editor({
       formData.append('imageFile', file);
       if (file !== null) {
         try {
-          const response = await axios.post('/api/s3/image', formData, {
+          await axios.post('http://localhost:8080/api/s3/image' , formData , {
+            headers: {
+              'content-type': 'multipart/form-data',
+              Authorization: accessToken,
+            }
+          })
+          .then((responseData) => {
+            const data = responseData.data.data;
+            seturl(data);
+          });
+/*           const response = await axios.post('/api/s3/image', formData, {
             headers: {
               'content-type': 'multipart/form-data',
               // enctype: 'multipart/form-data',
@@ -87,39 +96,44 @@ export default function Editor({
               Authorization: accessToken,
             },
           });
-          console.log(response.data.data);
-          seturl(response.data.data);
+          if(response !== null && response !== undefined && response !== ''  )
+          seturl(response.data.data); */
+/*           
+          const editor = QuillRef.current.getEditor(); 
+          // 2. 현재 에디터 커서 위치값을 가져온다
+          const range = editor.getSelection();
+          // 가져온 위치에 이미지를 삽입한다
+          editor.insertEmbed(range.index, 'image', `${url}`);  */
 
-          // console.log(seturl);
-            /* const range = QuillRef.current?.getEditor().getSelection()?.index;
+          const range = QuillRef.current?.getEditor().getSelection()?.index;
           if (range !== null && range !== undefined) {
             const quill = QuillRef.current?.getEditor();
 
             quill?.setSelection(range, 1);
 
-            quill?.clipboard.dangerouslyPasteHTML(range, `<img src=${url2} alt='asas' />`);
-          } 
-          return { ...response, success: true }; */
-          if(url !== ''){
-
-            const editor = QuillRef.current.getEditor(); 
-    
-            // 2. 현재 에디터 커서 위치값을 가져온다
-            const range = editor.getSelection();
-            // 가져온 위치에 이미지를 삽입한다
-            editor.insertEmbed(range.index, 'image', `${url}`);
+            quill?.clipboard.dangerouslyPasteHTML(
+              range,
+              `<img src=${url} alt="이미지 태그가 삽입됩니다." />`
+            );
           }
-          
-        } 
-        
+        }
         catch (error) {
           const err = error;
           return { ...err.response, success: false };
         }
       }
-      return ''
+      return '';
     };
   };
+
+ /*  useEffect(() => { 
+      const editor = QuillRef.current.getEditor(); 
+      // 2. 현재 에디터 커서 위치값을 가져온다
+      const range = editor.getSelection();
+      // 가져온 위치에 이미지를 삽입한다
+      editor.insertEmbed(range.index, 'image', `${url}`); 
+  }, [url]); */
+
 
   const modules = useMemo(() => ({
     toolbar: {
